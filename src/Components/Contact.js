b06d244d8cc24ae564595859a5e2e354003f7e0e
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-07-06 16:54:15 +0400

    Routing

---
+++ src/Components/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Contact(props) {
+    return(
+        <div>
+            <h1>Contact</h1>
+        </div>
+    )
+}
---
+++ src/Components/Menu.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Menu(props) {
+    return(
+        <div>
+            <h1>Menu</h1>
+        </div>
+    )
+}
