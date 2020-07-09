35ea968b0e0577567a1db2d3d1ea4aa34dba7047
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-07-09 23:44:12 +0400

    Updates

---
+++ precache-manifest.ec650e7312e3d48f41a2d7db6019b762.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66de05da73364a3c97976eb9caca416c",
+    "url": "/restoran/index.html"
+  },
+  {
+    "revision": "cb1d058fe9715707217f",
+    "url": "/restoran/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "58da39946d599f25a0a8",
+    "url": "/restoran/static/css/main.a274114a.chunk.css"
+  },
+  {
+    "revision": "cb1d058fe9715707217f",
+    "url": "/restoran/static/js/2.2a5c8e72.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/restoran/static/js/2.2a5c8e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58da39946d599f25a0a8",
+    "url": "/restoran/static/js/main.f007e622.chunk.js"
+  },
+  {
+    "revision": "00a78d55f0fcc991519a",
+    "url": "/restoran/static/js/runtime-main.dda13aa5.js"
+  },
+  {
+    "revision": "39ed072b6e80d3d318d08df4eefb898b",
+    "url": "/restoran/static/media/Screenshot_1.39ed072b.jpg"
+  },
+  {
+    "revision": "6728091903a01d49a76342ad6128a73e",
+    "url": "/restoran/static/media/Screenshot_13.67280919.jpg"
+  },
+  {
+    "revision": "2f5556e94b847738812fbe3a51dbf52f",
+    "url": "/restoran/static/media/img.2f5556e9.jpg"
+  }
+]);
